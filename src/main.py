4633366747459d646012cb1e2c969609ca507665
Leandro Gabriel <45115495+VelocityIsntSpeed@users.noreply.github.com>
2019-12-13 01:16:01 -0300

    Add esboço de geração do grafo social

--- src/main.py
+++ src/main.py
@@ -1,18 +1,27 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import random as random
 
 print("test")
 
-# Criar grafo
-G = nx.Graph()
+nomes_de_pessoas = ['Ansley', 'Kyra', 'Irene', 'Abbas', 'Addalynn', 'Ariadne', 'Benicio', 'Gavyn', 'Millie', 'Kiyan', 'Emmett', 'Emelia', 'Marcus', 'Kylie', 'Lukas', 'Kenia', 'Zane', 'Elle', 'Abel', 'Marion', 'Kynslee', 'Aspyn', 'Charlize', 'Billy', 'Selina', 'Reyna', 'Louisa', 'Braelyn', 'Lauren', 'Evander', 'Kennedi', 'Amir', 'Gianni', 'Bryton', 'Isaac', 'Walter', 'Sahana', 'Elly', 'Armani', 'Shiloh', 'Emma', 'Ariel', 'Truett', 'Meredith', 'Mayte', 'Regina', 'Emi', 'Celina', 'April', 'Ruger', 'Russell', 'Avah', 'Landry', 'Avril', 'Hailie', 'Journie', 'Reyna', 'Isha', 'Veer', 'Anika', 'Kerrigan', 'Clay', 'Leonard', 'Ammon', 'Aida', 'Henley', 'Tate', 'Adyson', 'Ansley', 'Mahdi', 'Alexander', 'Ameer', 'Montgomery', 'Laith', 'Lochlan', 'Hanna', 'Harper', 'Latrell', 'Kenzi', 'Dan', 'Guillermo', 'Brad', 'Julieta', 'Rilynn', 'Alicia', 'Rocco', 'Osiris', 'Arden', 'Mitchell', 'Eva', 'Mireya', 'Brylee', 'Westin', 'Emmanuel', 'Dalton', 'Gavriel', 'Marlena']
 
-# Add vertices
-G.add_nodes_from(['Lucas', 'Maria', 'Pedro', 'Pedro', 'Ana', 'Renato'])
-# Note que o pedro so eh adicionado uma vez.
+def gen_social_graph(size = 12, amount_connections = 20):
+    """Gera um grafo social."""
 
-# Add arestas
-G.add_edges_from([('Lucas', 'Maria'), ('Pedro', 'Maria'), ('Lucas', 'Pedro'), ('Maria', 'Renato')])
+    # Criar grafo
+    G = nx.Graph()
+
+    # Add vertices
+    G.add_nodes_from(random.choices(nomes_de_pessoas, k=size))
+
+    # Add arestas
+    for i in range(amount_connections):
+        pair = random.choices(list(G.nodes), k=2)
+        G.add_edge(pair[0], pair[1])
+    
+    return G
 
 # Abrir visualizacao interativa com o matplotlib
-nx.draw(G, with_labels=True)
+nx.draw(gen_social_graph(12, 10), with_labels = True, font_weight='bold')
 plt.show()
