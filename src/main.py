4e93bc53adaf3caea4aa4aa3520c74cd7a377cee
Leandro Gabriel <45115495+VelocityIsntSpeed@users.noreply.github.com>
2019-12-13 03:21:25 -0300

    Coloca visualização numa função

--- src/main.py
+++ src/main.py
@@ -8,7 +8,12 @@ from achar_cliques_relevantes import achar_cliques_relevantes
 
 
 
-# Abrir visualizacao interativa com o matplotlib
-G = gerar_grafo_social(12, 10)
-nx.draw(G, with_labels = True, font_weight='bold')
-plt.show()
+def visualizar(grafo):
+    """
+    Abre uma visualizacao interativa do grafo com o matplotlib
+    """
+    # TODO: Mostrar opinioes
+    nx.draw(grafo, with_labels = True, font_weight='bold')
+    plt.show()
+
+visualizar(gerar_grafo_social(20, 18))
