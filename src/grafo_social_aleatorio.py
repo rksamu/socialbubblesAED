5ae0738364ba70d551ae58fcd0f6eec8e9a69949
Leandro Gabriel <45115495+VelocityIsntSpeed@users.noreply.github.com>
2019-12-17 00:37:53 -0300

    Update grafo_social_aleatorio.py

--- src/grafo_social_aleatorio.py
+++ src/grafo_social_aleatorio.py
@@ -2,12 +2,11 @@ import networkx as nx
 import random as random
 from nomes import lista_de_nomes
 
-def grafo_social_aleatorio(tamanho = 12, qtd_conexoes = 10):
+def _basico(tamanho = 12, qtd_conexoes = 10):
     """
-    Gera um grafo social aleatoriamente.
-    O grafo tem quantidade de nós e arestas determinadas pelos argumentos.
+    As arestas são aleatoriamente escolhidas dentre todas as arestas possíveis.
+    As opiniões são uniformemente aleatórias sem relação alguma com a topologia.
     """
-
     # Criar grafo
     G = nx.Graph()
 
@@ -25,3 +24,6 @@ def grafo_social_aleatorio(tamanho = 12, qtd_conexoes = 10):
         G.add_edge(par[0], par[1])
     
     return G
+
+def grafo_social_aleatorio(tamanho = 12, qtd_conexoes = 10):
+    return _basico(tamanho, qtd_conexoes)
