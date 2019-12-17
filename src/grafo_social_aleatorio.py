0bea41a1223c552a4ec696d3896d57a1fe04b684
Leandro Gabriel <45115495+VelocityIsntSpeed@users.noreply.github.com>
2019-12-16 21:50:50 -0300

    Muda lista de nomes para uma com 500 nomes curtos em um arquivo separado

--- src/grafo_social_aleatorio.py
+++ src/grafo_social_aleatorio.py
@@ -1,7 +1,6 @@
 import networkx as nx
 import random as random
-
-nomes_de_pessoas = ['Ansley', 'Kyra', 'Irene', 'Abbas', 'Addalynn', 'Ariadne', 'Benicio', 'Gavyn', 'Millie', 'Kiyan', 'Emmett', 'Emelia', 'Marcus', 'Kylie', 'Lukas', 'Kenia', 'Zane', 'Elle', 'Abel', 'Marion', 'Kynslee', 'Aspyn', 'Charlize', 'Billy', 'Selina', 'Reyna', 'Louisa', 'Braelyn', 'Lauren', 'Evander', 'Kennedi', 'Amir', 'Gianni', 'Bryton', 'Isaac', 'Walter', 'Sahana', 'Elly', 'Armani', 'Shiloh', 'Emma', 'Ariel', 'Truett', 'Meredith', 'Mayte', 'Regina', 'Emi', 'Celina', 'April', 'Ruger', 'Russell', 'Avah', 'Landry', 'Avril', 'Hailie', 'Journie', 'Reyna', 'Isha', 'Veer', 'Anika', 'Kerrigan', 'Clay', 'Leonard', 'Ammon', 'Aida', 'Henley', 'Tate', 'Adyson', 'Ansley', 'Mahdi', 'Alexander', 'Ameer', 'Montgomery', 'Laith', 'Lochlan', 'Hanna', 'Harper', 'Latrell', 'Kenzi', 'Dan', 'Guillermo', 'Brad', 'Julieta', 'Rilynn', 'Alicia', 'Rocco', 'Osiris', 'Arden', 'Mitchell', 'Eva', 'Mireya', 'Brylee', 'Westin', 'Emmanuel', 'Dalton', 'Gavriel', 'Marlena']
+from nomes import lista_de_nomes
 
 def grafo_social_aleatorio(tamanho = 12, qtd_conexoes = 10):
     """
@@ -13,7 +12,7 @@ def grafo_social_aleatorio(tamanho = 12, qtd_conexoes = 10):
     G = nx.Graph()
 
     # Add vertices
-    G.add_nodes_from(random.choices(nomes_de_pessoas, k=tamanho))
+    G.add_nodes_from(random.choices(lista_de_nomes, k=tamanho))
 
     # Add opinioes
     for node in G.nodes:
